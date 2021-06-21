88fc98d5247df683a25b61d10b6ebd8562e263fa
yl9019 <60221798+yl9019@users.noreply.github.com>
2021-06-22 03:23:03 +0800

    Update update_ui.js

--- Final/Command/command_app/web_page/update_ui.js
+++ Final/Command/command_app/web_page/update_ui.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 
 function fillObstacle(document, oIds) {
   for (var i = 0; i < oIds.length; i++) {
